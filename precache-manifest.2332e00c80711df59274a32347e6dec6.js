f3d874d804c744a9167ea10990a50f2f42623357
Jinhwa Roh <nnjh12@gmail.com>
2019-06-22 22:56:08 -0400

    Updates

---
+++ precache-manifest.2332e00c80711df59274a32347e6dec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29131ea1c1b6890bd70b5f7d72415c0",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "bdf9459e7713a67d3612",
+    "url": "/clickygame/static/css/main.573f757e.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "bdf9459e7713a67d3612",
+    "url": "/clickygame/static/js/main.44a636a4.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
